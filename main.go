e69f95838083f983b52de313042dae8b37e47ffc
Greg Pearce <gregrpearce@gmail.com>
2017-03-26 22:16:29 +0100

    start adding firebase code

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 
 var (
 	db     *sql.DB
+	firebaseKey string
 )
 
 func main() {
@@ -27,11 +28,15 @@ func main() {
 
 	var err error
 
+	// open database connection
 	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL") + " sslmode=disable")
 	if err != nil {
 		log.Fatalf("Error opening database: %q", err)
 		panic(err)
 	}
+
+	// load firebase key
+	firebaseKey = os.Getenv("FIREBASE_KEY")
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -107,6 +112,10 @@ func updateLineStatus(c *gin.Context) {
 	insertInDatabase(c, linesJson)
 
 	printLines(c, linesJson)
+
+	// send notification
+	lines := decodeLinesJson(linesJson)
+
 }
 
 func fetchLinesJson() (string, error) {
@@ -155,6 +164,37 @@ func insertInDatabase(c *gin.Context, linesJson string) {
 			fmt.Sprintf("database error: %q", err))
 		return
 	}
+}
+
+func sendLineStatusNotification(lineId string, status string) (string, error) {
+	url := "https://fcm.googleapis.com/fcm/send"
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return "", err
+	}
+
+	req.Header.Add("Authorization", "key=" + firebaseKey)
+	req.Header.Add("Content-Type", "application/json")
+	
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("ReadAll: ", err)
+		return "", err
+	}
+	json := string(bytes)
+
+	return json, nil
 }
 
 type Line struct {
